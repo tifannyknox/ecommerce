8f420944bf64b6a3ee526a7d351059baea2836b6
Danubia <danyssoares@hotmail.com>
2018-08-26 22:39:23 -0300

    Sucesso

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -11,13 +12,16 @@ $app->config('debug', true);
 
 $app->get('/', function () {
 
-//    $sql = new Hcode\DB\Sql();
-//
-//    $results = $sql->select("SELECT * FROM tb_users");
-//
-//    echo json_encode($results);
-
     $page = new Page();
+
+    $page->setTpl("index");
+
+});
+
+
+$app->get('/admin', function () {
+
+    $page = new PageAdmin();
 
     $page->setTpl("index");
 
